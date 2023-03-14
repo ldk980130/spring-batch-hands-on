bf3b3676482faf545ec8873f467591d16130545d
does <ldk980130@gmail.com>
2023-03-14 18:22:11 +0900

    initial commit

---
+++ src/main/java/com/example/springbatchhandson/SpringBatchHandsOnApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbatchhandson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchHandsOnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchHandsOnApplication.class, args);
+    }
+
+}
