c212d3436a0a86bd569f1d4bafdf0b9758f9da4f
does <ldk980130@gmail.com>
2023-03-23 13:20:37 +0900

    feat: jobParameters 주입으로 jobInstance와 jobExcution 차이 이해하기

--- src/main/java/com/example/springbatchhandson/job/SimpleBatchConfig.java
+++ src/main/java/com/example/springbatchhandson/job/SimpleBatchConfig.java
@@ -4,10 +4,12 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -23,16 +25,17 @@ public class SimpleBatchConfig {
     @Bean
     public Job simpleJob() {
         return new JobBuilder("simpleJob", jobRepository)
-                .start(simpleStep1())
+                .start(simpleStep1(null))
                 .build();
-
     }
 
     @Bean
-    public Step simpleStep1() {
+    @JobScope
+    public Step simpleStep1(@Value("#{jobParameters[requestDate]}") String requestDate) {
         return new StepBuilder("simpleStep", jobRepository)
                 .tasklet((contribution, chunkContext) -> {
                             log.info(">>>>> This is Step1");
+                            log.info(">>>>> requestDate = {}", requestDate);
                             return RepeatStatus.FINISHED;
                         }, platformTransactionManager
                 )
